83ff0e6bb04f6c8a6b5cf2ef33c952070f97b0f7
gabrieltsouza <gtsouza.gt@gmail.com>
2023-08-29 01:47:58 -0300

    commit

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
+
+// Form
+import { FaPlus } from 'react-icons/fa';
+
+// Tasks
+import { FaEdit, FaWindowClose } from 'react-icons/fa';
+
 import './Main.css';
 
 export default class Main extends Component {
   state = {
     newTask: '',
+    tasks: [],
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { tasks } = this.state;
+    let { newTask } = this.state;
+    newTask = newTask.trim();
+
+    if (tasks.indexOf(newTask) !== -1) return;
+
+    const newTasks = [...tasks];
+
+    this.setState({
+      tasks: [...newTasks, newTask],
+    });
   };
 
   handleInputChanged = (e) => {
@@ -13,14 +36,32 @@ export default class Main extends Component {
   };
 
   render() {
-    const { newTask } = this.state;
+    const { newTask, tasks } = this.state;
     return (
       <div className="main">
         <h1>Task List {newTask} </h1>
-        <form action="#">
-          <input onChange={this.handleInputChanged} type="text" />
-          <button type="submit">Send</button>
+        <form onSubmit={this.handleSubmit} action="#" className="form">
+          <input
+            onChange={this.handleInputChanged}
+            type="text"
+            value={newTask}
+          />
+          <button type="submit">
+            <FaPlus></FaPlus>
+          </button>
         </form>
+
+        <ul className="tasks">
+          {tasks.map((task) => (
+            <li key={task}>
+              {task}
+              <span>
+                <FaEdit className="edit"></FaEdit>
+                <FaWindowClose className="delete"></FaWindowClose>
+              </span>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
