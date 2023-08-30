09b28bc8b1003b86918a6e7c635f5c755623a234
Gabriel Souza <20734245+gabrieltsouza@users.noreply.github.com>
2023-08-30 18:58:50 -0300

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,33 @@
 import React, { Component } from 'react';
 
-// Form
-import { FaPlus } from 'react-icons/fa';
-
-// Tasks
-import { FaEdit, FaWindowClose } from 'react-icons/fa';
-
+import Form from './Form';
+import Tasks from './Tasks';
 import './Main.css';
 
 export default class Main extends Component {
   state = {
     newTask: '',
     tasks: [],
+    index: -1,
   };
+
+  componentDidMount() {
+    const tasks = JSON.parse(localStorage.getItem('tasks'));
+    if (!tasks) return;
+
+    this.setState({ tasks: tasks });
+  }
+  componentDidUpdate(prevProps, prevState) {
+    const { tasks } = this.state;
+
+    if (tasks === prevState.tasks) return;
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
 
   handleSubmit = (e) => {
     e.preventDefault();
-    const { tasks } = this.state;
+    const { tasks, index } = this.state;
     let { newTask } = this.state;
     newTask = newTask.trim();
 
@@ -24,9 +35,12 @@ export default class Main extends Component {
 
     const newTasks = [...tasks];
 
-    this.setState({
-      tasks: [...newTasks, newTask],
-    });
+    if (index === -1) {
+      this.setState({ tasks: [...newTasks, newTask], newTask: '' });
+    } else {
+      newTasks[index] = newTask;
+      this.setState({ tasks: [...newTasks], index: -1 });
+    }
   };
 
   handleInputChanged = (e) => {
@@ -35,33 +49,34 @@ export default class Main extends Component {
     });
   };
 
+  handleDelete = (e, index) => {
+    const { tasks } = this.state;
+    const tasks2Change = [...tasks];
+    tasks2Change.splice(index, 1);
+    this.setState({ tasks: [...tasks2Change] });
+  };
+
+  handleEdit = (e, index) => {
+    const { tasks } = this.state;
+    this.setState({ index, newTask: tasks[index] });
+  };
+
   render() {
     const { newTask, tasks } = this.state;
     return (
       <div className="main">
         <h1>Task List {newTask} </h1>
-        <form onSubmit={this.handleSubmit} action="#" className="form">
-          <input
-            onChange={this.handleInputChanged}
-            type="text"
-            value={newTask}
-          />
-          <button type="submit">
-            <FaPlus></FaPlus>
-          </button>
-        </form>
 
-        <ul className="tasks">
-          {tasks.map((task) => (
-            <li key={task}>
-              {task}
-              <span>
-                <FaEdit className="edit"></FaEdit>
-                <FaWindowClose className="delete"></FaWindowClose>
-              </span>
-            </li>
-          ))}
-        </ul>
+        <Form
+          handleSubmit={this.handleSubmit}
+          handleInputChanged={this.handleInputChanged}
+          newTask={newTask}
+        />
+        <Tasks
+          handleEdit={this.handleEdit}
+          handleDelete={this.handleDelete}
+          tasks={tasks}
+        />
       </div>
     );
   }
